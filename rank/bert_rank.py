9f867b19bafa80c469f4b1a3793bbf601bf11819
Handsomecao <cs18281902202@gmail.com>
2019-05-14 11:13:41 +0800

    first commit

---
+++ rank/bert_rank.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# 通过训练好的BERT二分类网络，选择相关的一些文档作为候选文档。
+
+import torch
+import os
+import json
+import numpy as np
+import re
+import torch.nn.functional as F
+from tqdm import tqdm
+from collections import defaultdict
+from pytorch_pretrained_bert import BertForSequenceClassification, BertConfig, BertTokenizer
+from run_classifier import InputExample, InputFeatures, convert_examples_to_features
